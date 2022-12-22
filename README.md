dd4cbd99514dcbd2adf560dae830658e9390be59
AndreiRookie <andrey.idem@gmail.com>
2022-12-22 11:27:06 +0400

    Main.java & README.md added

---
+++ README.md
@@ -0,0 +1,8 @@
+## SALES MANAGER
+**Each *sales manager* has his own sales.**
+
+This method let you know the max sale of a *sales manager*:
+
+```java
+public int max()
+```
---
+++ src/Main.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+
+        SalesManager newManager;
+        int[] weekSales = {2, 4, 7, 3, 5};
+        newManager = new SalesManager(weekSales);
+
+        System.out.println("newManager's week sales: " + Arrays.toString(newManager.sales));
+        System.out.println("newManager's max sale is: " + newManager.max());
+    }
+}
