aabf62f83a42645bd2376209ce226010b8fce6ca
AndreiRookie <andrey.idem@gmail.com>
2022-12-23 20:07:20 +0400

    trimmedMean() & min() added

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,25 @@
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
 
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите количество продаж в день нового менеджера через пробел:");
+        System.out.print(">>>");
+
+        String[] input = scanner.nextLine().split(" ");
+        int[] salesCount = new int[input.length];
+        for (int i = 0; i < input.length; i++) {
+            salesCount[i] = Integer.parseInt(input[i]);
+        }
+
         SalesManager newManager;
-        int[] weekSales = {2, 4, 7, 3, 5};
-        newManager = new SalesManager(weekSales);
+        newManager = new SalesManager(salesCount);
 
         System.out.println("newManager's week sales: " + Arrays.toString(newManager.sales));
         System.out.println("newManager's max sale is: " + newManager.max());
+        System.out.println("newManager's min sale is: " + newManager.min());
+        System.out.println("newManager's trimmed mean: " + newManager.trimmedMean());
     }
 }
